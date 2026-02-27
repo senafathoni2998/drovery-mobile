0c99babb9921707375f46edb1f0db85f5d249b4b
senaahmad2998 <senaahmad2998@gmail.com>
2026-02-28 01:42:25 +0700

    Fix styles import to use alias in LogoutButton

--- features/profile/screens/ProfileScreen/components/LogoutButton.tsx
+++ features/profile/screens/ProfileScreen/components/LogoutButton.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { spacing, borderRadius, colors } from "../../../../../styles/common";
+import { spacing, borderRadius, colors } from "@/styles/common";
 
 interface LogoutButtonProps {
   onPress: () => void;
