92cecbaacf0de546ebb5d1931f9c5747b7cdd547
senaahmad2998 <senaahmad2998@gmail.com>
2026-02-28 01:42:16 +0700

    Fix styles import to use alias in FooterActions

--- features/delivery/screens/DeliveryDetailScreen/components/FooterActions.tsx
+++ features/delivery/screens/DeliveryDetailScreen/components/FooterActions.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
-import { spacing, borderRadius, colors } from "../../../../../styles/common";
+import { spacing, borderRadius, colors } from "@/styles/common";
 
 interface FooterActionsProps {
   onTrackMap?: () => void;
