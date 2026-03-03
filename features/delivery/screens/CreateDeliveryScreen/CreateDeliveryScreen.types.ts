4b068878616c1db91fed5db4473aabd8a3fa96eb
senaahmad2998 <senaahmad2998@gmail.com>
2026-03-03 21:52:59 +0700

    Add CreateDeliveryScreen type definitions

---
+++ features/delivery/screens/CreateDeliveryScreen/CreateDeliveryScreen.types.ts
@@ -0,0 +1,31 @@
+export interface CreateDeliveryFormData {
+  from: string;
+  to: string;
+  receiver: string;
+  packages: string;
+  packageSize: string;
+  packageWeight: string;
+  pickupDate: string;
+  pickupTime: string;
+  packageTypes: string[];
+}
+
+export interface PackageType {
+  id: string;
+  label: string;
+  icon: keyof typeof MaterialIcons.glyphMap;
+}
+
+export interface FormErrors {
+  from?: string;
+  to?: string;
+  receiver?: string;
+  packages?: string;
+  packageSize?: string;
+  packageWeight?: string;
+  pickupDate?: string;
+  pickupTime?: string;
+  packageTypes?: string;
+}
+
+import { MaterialIcons } from "@expo/vector-icons";
