027e212ec429b08a55f83f3d724a1bd36af9131d
senaahmad2998 <senaahmad2998@gmail.com>
2026-03-07 16:15:32 +0700

    Export EditProfileScreen, PaymentMethodsScreen, HelpSupportScreen, and TermsPrivacyScreen from screens index

--- features/profile/screens/index.ts
+++ features/profile/screens/index.ts
@@ -1,1 +1,5 @@
 export { ProfileScreen } from './ProfileScreen/ProfileScreen';
+export { EditProfileScreen } from './EditProfileScreen';
+export { PaymentMethodsScreen } from './PaymentMethodsScreen';
+export { HelpSupportScreen } from './HelpSupportScreen';
+export { TermsPrivacyScreen } from './TermsPrivacyScreen';
